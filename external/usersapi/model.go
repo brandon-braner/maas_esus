c8caa8120792dc01058f83d9c890b1d6c2c0e6e5
Brandon Braner <brandon.braner@gmail.com>
2024-12-30 17:21:29 -0600

    wip on generic mongo repository

--- external/usersapi/model.go
+++ external/usersapi/model.go
@@ -1,1 +1,29 @@
 package usersapi
+
+import (
+	"github.com/brandonbraner/maas/pkg/permissions"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID          primitive.ObjectID      `bson:"_id,omitempty"`
+	Username    string                  `json:"username" bson:"username"`
+	Password    string                  `json:"password" bson:"password"`
+	Firstname   string                  `json:"firstname" bson:"firstname"`
+	Lastname    string                  `json:"lastname" bson:"lastname"`
+	Permissions permissions.Permissions `json:"permissions" bson:"permissions"`
+	Tokens      int                     `json:"tokens" bson:"tokens"`
+}
+
+// NewUser is a constructor function for creating a new User instance
+func NewUser(username, password, firstname, lastname string, perms permissions.Permissions, tokens int) *User {
+	return &User{
+		ID:          primitive.NewObjectID(), // Automatically generate a new ObjectID
+		Username:    username,
+		Password:    password,
+		Firstname:   firstname,
+		Lastname:    lastname,
+		Permissions: perms,
+		Tokens:      tokens,
+	}
+}
